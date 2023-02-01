500504d83d7732d461ad0fe8116dc8dd2b9033d5
Radon222 <89600373712@mail.ru>
2023-02-01 12:29:35 +0300

    init

---
+++ src/Todolist.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Todolist() {
+  return (
+    <div>
+      <ul>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+      </ul>
+    </div>
+  );
+}
